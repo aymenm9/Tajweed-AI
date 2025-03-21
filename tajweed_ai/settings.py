e806953bf24877fffd6f7b69122b6195cdf79795
aymen merad <ay28mene@gmail.com>
2025-03-21 01:06:11 +0100

    docker and deploy

--- tajweed_ai/settings.py
+++ tajweed_ai/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ebgewi6tow+*rb@t_vgwlr=!j#xnqx8(2zt+(@_#zqbm3w9pw#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
